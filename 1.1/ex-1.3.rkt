3d5fc38009b04afba890cc465b42e320f3b512ba
imsamuel <samuelywp@gmail.com>
2021-07-10 13:42:15 +0800

    refactor: add line break before definitions

--- 1.1/ex-1.3.rkt
+++ 1.1/ex-1.3.rkt
@@ -1,9 +1,11 @@
 #lang racket/base
 ; Define a procedure that takes three numbers as arguments and returns the sum 
 ; of the squares of the two larger numbers.
-(define (square x) (* x x))
+(require "../common.rkt")
 
 (define (sum-of-squares a b c)
   (cond ((and (< a b) (< a c)) (+ (square b) (square c)))
         ((and (< b a) (< b c)) (+ (square a) (square c)))
         (else (+ (square a) (square b)))))
+
+(sum-of-squares 10 0 5)
