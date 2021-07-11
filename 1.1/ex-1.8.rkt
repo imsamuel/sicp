f10ce15d115fa2ed199e08f39e69a15749b6fca0
imsamuel <samuelywp@gmail.com>
2021-07-11 13:37:26 +0800

    feat: add ex 1.8 implementation

--- 1.1/ex-1.8.rkt
+++ 1.1/ex-1.8.rkt
@@ -3,10 +3,21 @@
 ; approximation to the cube root of x, then a better approximation is given by 
 ; the value (x/y^2 + 2y) / 3. Use this formula to implement a cube-root procedure 
 ; analogous to the square-root procedure.
-
-(define (cbrt x) 0)
+(require "../common.rkt")
 
 (define (improve guess x)
-  (+ (/ x
-        ()))
+  (/ (+ (/ x
+           (square guess))
+        (* 2 guess))
+     3))
+
+(define (good-enough guess x)
+  (<= (abs (- x (cube guess))) 0.001))
+
+(define (cbrt-iter guess x)
+  (cond ((good-enough guess x) guess)
+  (else (cbrt-iter (improve guess x) x))))
+
+(define (cbrt x)
+  (cbrt-iter 1 x))
 
