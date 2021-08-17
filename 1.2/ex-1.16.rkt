7c8647da88e4abc7d1010d34c7f6e6dbb1c05870
imsamuel <samuelywp@gmail.com>
2021-08-17 13:23:41 +0800

    docs: edit problem introduction

--- 1.2/ex-1.16.rkt
+++ 1.2/ex-1.16.rkt
@@ -1,7 +1,7 @@
 #lang racket/base
 ; Design a procedure that evolves an iterative exponentiation process that uses
 ; successive squaring and uses a logarithmic number of steps, as does fast-expt.
-; (Hint: Using the observation that (bn/2) ^ 2 = (b2)^ n/2, keep, along with
+; (Hint: Using the observation that (b^(n/2))^2 = (b^2)^(n/2), keep, along with
 ; the exponent n and the base b, an additional state variable a, and define the
 ; state transformation in such a way that the product abn is unchanged from
 ; state to state. At the beginning of the process a is taken to be 1, and the
